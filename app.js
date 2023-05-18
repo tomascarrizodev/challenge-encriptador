85da830cf710495128d96261d285b1e1b0be731b
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2023-05-18 13:36:28 -0300

    Nueva alerta de mayus y tildes

--- app.js
+++ app.js
@@ -1,66 +1,80 @@
 //! elementos visibles
-const header = document.querySelector('header')
+const $header = document.querySelector('header')
 
-const textarea = document.querySelector('#textarea')
+const $textarea = document.querySelector('#textarea')
 
-const answerContainer = document.querySelector('.answer-container')
+const $answerContainer = document.querySelector('.answer-container')
 
-const boyImage = document.querySelector('#boy')
-const noMensajeAviso = document.querySelector('.no-mensaje')
-const ingresaTextoAviso = document.querySelector('.ingresa-texto')
+const $boyImage = document.querySelector('#boy')
+const $noMensajeAviso = document.querySelector('.no-mensaje')
+const $ingresaTextoAviso = document.querySelector('.ingresa-texto')
 
-const respuesta = document.querySelector('#respuesta')
+const $respuesta = document.querySelector('#respuesta')
+
+const $alerta = document.querySelector('#alerta-mayus-tilde')
+const $btnAlerta = document.querySelector('#btn-alerta')
 
 //! elementos funcionales
-const btnPegar = document.querySelector('#pegar')
-const btnLimpiar = document.querySelector('#limpiar')
+const $btnPegar = document.querySelector('#pegar')
+const $btnLimpiar = document.querySelector('#limpiar')
 
-const btnEncriptar = document.querySelector('#btn-encriptar')
-const btnDesencriptar = document.querySelector('#btn-desencriptar')
+const $btnEncriptar = document.querySelector('#btn-encriptar')
+const $btnDesencriptar = document.querySelector('#btn-desencriptar')
 
-const btnCopyPaste = document.querySelector('#btn-copypaste')
+const $btnCopyPaste = document.querySelector('#btn-copypaste')
 
-const btnCopiar = document.querySelector('#btn-copiar')
+const $btnCopiar = document.querySelector('#btn-copiar')
 
 //! varibles
+let tilde = true
+let mayus = true
+const tildes = ['á', 'Á', 'é', 'É', 'í', 'Í', 'ó', 'Ó', 'ú', 'Ú']
 const vocales = ['a', 'e', 'i', 'o', 'u']
 const nuevasVocales = ['ai', 'enter', 'imes', 'ober', 'ufat']
 
 const recipe = [['a', 'ai'], ['e', 'enter'], ['i', 'imes'], ['o', 'ober'], ['u', 'ufat']]
 
 //! funciones
+function formatoIncorrecto() {
+    console.log('cambio')
+    let texto = $textarea.value;
+    if (texto.match(/[A-Z]/g))
+        console.log('Mayuscula')
+}
+
 function toggleElements(value) {
     let toggle
     value == true 
         ? toggle = 'display: none;' 
         : toggle = 'display: block;';
     
-    noMensajeAviso.style = toggle
-    ingresaTextoAviso.style = toggle
+    $noMensajeAviso.style = toggle
+    $ingresaTextoAviso.style = toggle
     if (screen.width >= 1024) {
-        boyImage.style = toggle
+        $boyImage.style = toggle
     }
 }
 
 function toggleBoyImage() {
     if (screen.width < 1024) {
-        boyImage.style = 'display: none'
-    } else if (screen.width >= 1024 && respuesta.innerHTML == '' ) {
-        boyImage.style = 'display: block'
+        $boyImage.style = 'display: none'
+    } else if (screen.width >= 1024 && $respuesta.innerHTML == '' ) {
+        $boyImage.style = 'display: block'
+        $answerContainer.style = 'justify-content: center;'
     }
 }
 
 function encriptar() {
-    let info = textarea.value
+    let texto = $textarea.value
 
-    if (info == '') {
+    if (texto == '') {
         alert('Ingrese un texto válido')
         return false
     } else {
         // elimina caracteres especiales
-        info = info.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        texto = texto.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 
-        let arr = [...info]
+        let arr = [...texto]
 
         for (let k = 0; k < recipe.length; k++) {
             for (let l = 0; l < arr.length; l++) {
@@ -72,26 +86,26 @@ function encriptar() {
 
         toggleElements(true)
 
+        $respuesta.innerHTML = arr.join('')
+        $btnCopiar.style = 'display: block;'
+        $answerContainer.style = 'justify-content: space-between;'
 
-        respuesta.innerHTML = arr.join('')
-        btnCopiar.style = 'display: block;'
-        answerContainer.style = 'justify-content: space-between;'
-        answerContainer.scrollIntoView({behavior: 'smooth'})
+        screen.width < 1024 && $answerContainer.scrollIntoView({behavior: 'smooth'})
     }
 }
 
 function desencriptar() {
-    let info = textarea.value
+    let texto = $textarea.value
 
-    if (info == '') {
+    if (texto == '') {
         alert('Ingrese un texto válido')
         return false
     } else {
-        info = info.toLowerCase()
+        texto = texto.toLowerCase()
         
         //* Solución a medias, se come letras o se come letras y no elimina las nuevasVocales
         /*
-        let arr = [...info]
+        let arr = [...texto]
         for (let i = 0; i < recipe.length; i++) {
             for (j = 0; j < arr.length; j++) {
                 if (arr[j] === recipe[i][0]) {
@@ -104,26 +118,26 @@ function desencriptar() {
             }
         }
         let words = arr.join('')
-        respuesta.innerHTML = words
+        $respuesta.innerHTML = words
         */
         
-        let parrafo = info
         for(i = 0; i < vocales.length; i++) {
-            parrafo = parrafo.replaceAll(recipe[i][1], recipe[i][0])
+            texto = texto.replaceAll(recipe[i][1], recipe[i][0])
         }
 
         toggleElements(true)
 
-        respuesta.innerHTML = parrafo
-        btnCopiar.style = 'display: block;'
-        answerContainer.style = 'justify-content: space-between;'
-        answerContainer.scrollIntoView({behavior: 'smooth'})
+        $respuesta.innerHTML = texto
+        $btnCopiar.style = 'display: block;'
+        $answerContainer.style = 'justify-content: space-between;'
+        
+        screen.width < 1024 && $answerContainer.scrollIntoView({behavior: 'smooth'})
     }
 }
 
 function copiarTexto() {
-    let text = respuesta.innerHTML
-    navigator.clipboard.writeText(text)
+    let texto = $respuesta.innerHTML
+    navigator.clipboard.writeText(texto)
         .then(() => {
             console.log('Texto copiado al portapapeles')
         })
@@ -136,8 +150,8 @@ function pegarTexto() {
     navigator.clipboard.readText()
         .then(text => {
             console.log('Texto del portapapeles:', text)
-            textarea.value = text
-            textarea.focus()
+            $textarea.value = text
+            $textarea.focus()
         })
         .catch(err => {
             console.error('Error al leer del portapapeles:', err)
@@ -145,19 +159,20 @@ function pegarTexto() {
 }
 
 function limpiarTexto() {
-    textarea.value = ''
-    textarea.focus()
+    $textarea.value = ''
+    $textarea.focus()
     toggleElements(false)
-    respuesta.innerHTML = ''
-    btnCopiar.style = 'display: none;'
-    answerContainer.style = 'justify-content: center;'
+    $respuesta.innerHTML = ''
+    $btnCopiar.style = 'display: none;'
+    if (screen.width >= 1024)
+        $answerContainer.style = 'justify-content: center;'
 }
 
 function copiarPegarTexto() {
-    if (respuesta.innerHTML != '') {
-        textarea.value = respuesta.innerHTML
-        textarea.focus()
-        header.scrollIntoView({behavior: 'smooth'})
+    if ($respuesta.innerHTML != '') {
+        $textarea.value = $respuesta.innerHTML
+        $textarea.focus()
+        screen.width < 1024 && $header.scrollIntoView({behavior: 'smooth'})
     }
 }
 
@@ -165,12 +180,37 @@ function copiarPegarTexto() {
 
 window.addEventListener('resize', toggleBoyImage)
 
-btnPegar.addEventListener('click', pegarTexto)
-btnLimpiar.addEventListener('click', limpiarTexto)
+$textarea.addEventListener('keyup', event => {
+    let texto = event.target.value
+    let arr = [...texto]
+    if (tilde || mayus) {
+        for (let i = 0; i < tildes.length; i++) {
+            for (let j = 0; j < arr.length; j++) {
+                if (arr[j] == tildes[i] || texto.match(/[A-Z]/g)) {
+                    $alerta.style.display = 'flex'
+                    tilde = false
+                    mayus = false
+                    break;
+                }
+            }
+            break
+        }
+    }
+} )
 
-btnEncriptar.addEventListener('click', encriptar)
-btnDesencriptar.addEventListener('click', desencriptar)
+$btnPegar.addEventListener('click', pegarTexto)
+$btnLimpiar.addEventListener('click', limpiarTexto)
 
-btnCopyPaste.addEventListener('click', copiarPegarTexto)
+$btnEncriptar.addEventListener('click', encriptar)
+$btnDesencriptar.addEventListener('click', desencriptar)
 
-btnCopiar.addEventListener('click', copiarTexto)
+$btnCopyPaste.addEventListener('click', copiarPegarTexto)
+
+$btnCopiar.addEventListener('click', copiarTexto)
+
+$btnAlerta.addEventListener('click', event => {
+    $alerta.style.display = 'none'
+    mayus = false
+    tilde = false
+    $textarea.focus()
+})
